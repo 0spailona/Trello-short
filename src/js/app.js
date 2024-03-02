5a172f6722dc47644042c3ab32955feaf3d920a5
Daria <n-d-p@mail.ru>
2024-03-03 00:49:15 +0300

    add dist

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import domController from "./domController";
-import SaveDataService from "./saveDataService";
-
-const stateService = new SaveDataService(localStorage);
-
-const controller = new domController(stateService)
-controller.addListeners()
